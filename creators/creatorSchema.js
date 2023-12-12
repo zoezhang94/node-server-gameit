0c482daad25bd5a03115867ffe7c43cb9a06e909
xuanchu jiang <xuanchujiang@gmail.com>
2023-12-12 16:47:37 -0500

    removed signUp creatorInfo

--- creators/creatorSchema.js
+++ creators/creatorSchema.js
@@ -2,19 +2,13 @@ import mongoose from 'mongoose';
 const { Schema } = mongoose;
 
 const creatorSchema = new Schema({
-    name: String,
-    games_count: Number,
-    positions: [{
-        name: String
-    }],
-    games: [{
-        name: String,
-        id: Number,
-    }],
     userAccount: {
         type: Schema.Types.ObjectId,
         ref: 'User'
-    }
+    },
+    reviews: [{
+        text: String,
+    }]
 }, { timestamps: true });
 
 
